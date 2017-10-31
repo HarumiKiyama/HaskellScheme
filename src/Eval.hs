d97c2eb1f54105d6226427cbc55914c8981e3c23
uruk <lucius0720@hotmail.com>
2017-10-31 23:20:18 +0800

    add parser

--- src/Eval.hs
+++ src/Eval.hs
@@ -1,2 +1,71 @@
 {-# LANGUAGE OverloadedStrings #-}
-import Data.Text as T
+import           Control.Applicative   hiding ((<|>))
+import           Data.Functor.Identity (Identity)
+import qualified Data.Text             as T
+import           LispVal
+import           Text.Parsec
+import           Text.Parsec.Expr
+import qualified Text.Parsec.Language  as Lang
+import           Text.Parsec.Text
+import qualified Text.Parsec.Token     as Tok
+
+lexer :: Tok.GenTokenParser T.Text () Identity
+lexer = Tok.makeTokenParser style
+
+style :: Tok.GenLanguageDef T.Text () Identity
+style =
+  Lang.emptyDef
+  { Tok.commentStart = "{-"
+  , Tok.commentEnd = "-}"
+  , Tok.commentLine = "--"
+  , Tok.opStart = Tok.opLetter style
+  , Tok.opLetter = oneOf ":!#$%%&*+./<=>?@\\^|-~"
+  , Tok.identStart = letter <|> oneOf "-+/*=|&><"
+  , Tok.identLetter = digit <|> letter <|> oneOf "?+=|&-/"
+  , Tok.reservedOpNames = ["'", "\""]
+  }
+
+reservedOp :: T.Text -> Parser ()
+reservedOp op = Tok.reservedOp lexer $ T.unpack op
+
+parseAtom :: Parser LispVal
+parseAtom = do
+  p <- m_identifier
+  return $ Atom $ T.pack p
+
+parseText :: Parser LispVal
+parseText = do
+  reservedOp "\""
+  p <- many1 $ noneOf "\""
+  reservedOp "\""
+  return $ String . T.pack $ p
+
+parseNumber :: Parser LispVal
+parseNumber = Number . read <$> many1 digit
+
+parseNegNum :: Parser LispVal
+parseNegNum = do
+  char '-'
+  d <- many1 digit
+  return $ Number . negate . read $ d
+
+parseList :: Parser LispVal
+parseList =
+  List . concat <$> Text.Parsec.many parseExpr `sepBy` (char ' ' <|> char '\n')
+
+parseSExp :: Parser LispVal
+parseSExp =
+  List . concat <$>
+  m_parens (Text.Parsec.many parseExpr `sepBy` (char ' ' <|> char '\n'))
+
+parseQuote :: Parser LispVal
+parseQuote = do
+  reservedOp "\'"
+  x <- parseExpr
+  return $ List [Atom "quote", x]
+
+parseReserved :: Parser LispVal
+parseReserved = do
+  reservedOp "'()" >> return Nil
+  <|> (reservedOp "#t" >> return (Bool True))
+  <|> (reservedOp "#f" >> return (Bool False))
--- src/LispVal.hs
+++ src/LispVal.hs
@@ -1,10 +1,21 @@
-{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE DeriveDataTypeable         #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
-import Data.Text as T
-import qualified Data.Map as Map
-import           Control.Monad.Except
-import Control.Monad.Reader
 
+module LispVal
+  ( LispVal(..)
+  , Eval(..)
+  , IFunc(..)
+  , EnvCtx
+  , LispException(..)
+  , showVal
+  ) where
+
+import qualified Data.Map             as Map
+import qualified Data.Text            as T
+import           Data.Typeable        (Typeable)
+
+import           Control.Monad.Except
+import           Control.Monad.Reader
 type EnvCtx = Map.Map T.Text LispVal
 
 newtype Eval a = Eval
@@ -21,8 +32,45 @@ data LispVal
            EnvCtx
   | Nil
   | Bool Bool
-  deriving (Typeable)
+  deriving (Typeable, Eq)
 
 data IFunc = IFunc
   { fn :: [LispVal] -> Eval LispVal
   }
+instance Eq IFunc where
+  (==) _ _ = False
+
+instance Show LispVal where
+  show = T.unpack . showVal
+
+showVal :: LispVal -> T.Text
+showVal val =
+  case val of
+    (Atom atom)     -> atom
+    (String str)    -> T.concat ["\"", str, "\""]
+    (Number num)    -> T.pack $ show num
+    (Bool True)     -> "#t"
+    (Bool False)    -> "#f"
+    Nil             -> "'()'"
+    (List contents) -> T.concat ["(", T.unwords $ showVal <$> contents, ")"]
+    (Fun _)         -> "(internal function)"
+    (Lambda _ _)    -> "(lambda function)"
+
+unwordsList :: [LispVal] -> T.Text
+unwordsList list = T.unwords $ showVal <$> list
+
+data LispException
+  = NumArgs Integer
+            [LispVal]
+  | LengthOfList T.Text
+                 Int
+  | ExpectedList T.Text
+  | TypeMismatch T.Text
+                 LispVal
+  | BadSpecialForm T.Text
+  | NotFunction LispVal
+  | UnboundVar T.Text
+  | Default LispVal
+  | PError String
+  | IOError T.Text
+  deriving (Typeable)
